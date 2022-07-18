29ae33424922470a546dbb1f540b0376420561ea
haeun <haeun3882@dsm.hs.kr>
2022-07-18 09:30:44 +0900

    🌧 :: 게시물 올리기 기능 구현

---
+++ Wetoo/app/src/main/java/com/example/wetoo/BoardData.java
@@ -0,0 +1,8 @@
+package com.example.wetoo;
+
+public class BoardData {
+    private static String[] title = new String[10000];
+    private static String[] id = new String[10000];
+    private static String[] content = new String[10000];
+    private static String[] date = new String[10000];
+}
